9d12261cf888842518d59652dd9e8e8a86985f49
jiash1998 <1459538273@qq.com>
2020-08-28 17:38:20 +0800

    0828Modify1

--- src/JsUtil/throttle.js
+++ src/JsUtil/throttle.js
@@ -1,29 +1,50 @@
 //函数节流
 //连续触发事件但是在 n 秒中只执行一次函数。节流会稀释函数的执行频率。
-export function throttle(fnc, wait, type) {
-  let previous = 0;
-  let timeout;
+// export function throttle(fnc, wait, type) {
+//   let previous = 0;
+//   let timeout;
+
+//   return function() {
+//     var context = this;
+//     var args = arguments;
+
+//     //时间戳执行，函数触发在开始阶段
+//     if (type === 1) {
+//       var now = Date.now();
+//       if (now - previous > wait) {
+//         fnc.apply(context, args);
+//         previous = now;
+//       }
+//     } else if (type === 2) {
+//     //定时器执行，函数触发在结束阶段
+//       if (!timeout) {
+//         timeout = setTimeout(() => {
+//           fnc.apply(context, args);
+//           timeout = null;
+//         }, wait);
+//         console.log(timeout);
+//       }
+//     }
+//   };
+// }
+
+export function throttle(func, wait, mustRun) {
+  var timeout,
+    startTime = new Date();
 
   return function() {
-    var context = this;
-    var args = arguments;
+    var context = this,
+      args = arguments,
+      curTime = new Date();
 
-    //时间戳执行，函数触发在开始阶段
-    if (type === 1) {
-      var now = Date.now();
-      if (now - previous > wait) {
-        fnc.apply(context, args);
-        previous = now;
-      }
-    } else if (type === 2) {
-    //定时器执行，函数触发在结束阶段
-      if (!timeout) {
-        timeout = setTimeout(() => {
-          fnc.apply(context, args);
-          timeout = null;
-        }, wait);
-        console.log(timeout);
-      }
+    clearTimeout(timeout);
+    // 如果达到了规定的触发时间间隔，触发 handler
+    if (curTime - startTime >= mustRun) {
+      func.apply(context, args);
+      startTime = curTime;
+      // 没达到触发间隔，重新设定定时器
+    } else {
+      timeout = setTimeout(func, wait);
     }
   };
 }
--- src/views/JSFun/eventDelegation.vue
+++ src/views/JSFun/eventDelegation.vue
@@ -14,37 +14,66 @@
 </template>
 
 <script>
+// import throttle from "../../JsUtil/throttle";
 export default {
   name: "eventDelegation",
   data() {
     return {
-      list: [1, 2, 3],
+      list: [1, 2, 3, 4],
     };
   },
   mounted() {
-    // this.addElement(4);
-    // this.getTop();
+    let father = this.$qs("#eventDelegation");
+    let div1 = this.$qs("#d1");
+    let d4 = this.$qs("#d4");
+
     let count = document.getElementsByTagName("div");
-    console.log(count.length);
+    // console.log(count.length);
     this.init();
-    window.addEventListener("scroll", () => {
-      let cont = this.calcScreen();
-      for (let i = 1, len = this.list.length; i <= len; i++) {
-        if (document.getElementById("d" + i)) {
-          //   console.log("1");
-        //   document.getElementById("d" + i).style.cssText +=
-        //     "background-color:yellow;";
-        }
-      }
-    });
+    window.addEventListener(
+      "scroll",
+      this.throttle(this.windowEvent, 500, 1000)
+    );
+    // this.windowEvent();
+    // throttle(this.windowEvent, 500, 1000);
+    // this.H5NewApi(this.$qs("#d1"));
+    // console.log(div1,this.$qs("#d1"));
+    this.H5NewApi(this.$qs("#d4"));
+  },
+  destroyed() {
+    console.log("byb11");
   },
   methods: {
+    $qs(name) {
+      return document.querySelector(name);
+    },
+    realFunc() {
+      console.log("Success");
+    },
+    throttle(func, wait, mustRun) {
+      var timeout,
+        startTime = new Date();
+
+      return function () {
+        var context = this,
+          args = arguments,
+          curTime = new Date();
+
+        clearTimeout(timeout);
+        // 如果达到了规定的触发时间间隔，触发 handler
+        if (curTime - startTime >= mustRun) {
+          func.apply(context, args);
+          startTime = curTime;
+          // 没达到触发间隔，重新设定定时器
+        } else {
+          timeout = setTimeout(func, wait);
+        }
+      };
+    },
+    //基本信息
     calcScreen() {
       let clientH = document.documentElement.clientHeight;
-      let clientW = document.documentElement.clientWidth;
       let scrollT = document.documentElement.scrollTop;
-
-      //   console.log(clientH, scrollT);
       return { clientH, scrollT };
     },
     getTop() {
@@ -52,8 +81,35 @@ export default {
         console.log(document.getElementById("d" + i).offsetTop);
       }
     },
+    //监听滚动
+    windowEvent() {
+      let cont = this.calcScreen();
+      console.log(cont.scrollT);
+    },
+    //H5 新api 监听交叉区
+    H5NewApi(name) {
+      let ob = new IntersectionObserver(this.callback, {
+        threshold: [0, 0.25, 0.5, 0.75, 1],
+      });
+
+      ob.observe(name);
+    },
+    callback(entries) {
+      console.log(entries[0].target);
+      console.log(entries[0].intersectionRatio);
+      if (
+        entries[0].intersectionRatio > 0.25 &&
+        entries[0].intersectionRatio < 0.5
+      ) {
+        console.log("start");
+        this.addElement(0);
+      }
+    },
+    //创建节点
     addElement(i) {
       let div0 = document.createElement("div");
+      let father = this.$qs("#eventDelegation");
+
       div0.id = "d" + i;
       div0.innerHTML = i;
       div0.style.cssText = `width: 90%;
@@ -62,7 +118,7 @@ export default {
         background-color: pink;
         margin: 10px auto;
         transition: 4s;`;
-      document.body.appendChild(div0);
+      father.appendChild(div0);
     },
     init() {
       for (let i = 1, len = this.list.length; i <= len; i++) {
